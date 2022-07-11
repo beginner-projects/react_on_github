c76e5e2cd424719ae39354de9a02aef8ba813c9a
mitullgadhiya <mrab0709@gmail.com>
2022-07-11 13:36:38 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Bitcoin from './logo_btc.jpg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={Bitcoin} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Bitcoin !
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn about Blockchain
         </a>
       </header>
     </div>
